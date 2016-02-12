d3783244f6561807b7eebfb9278f3ba6f48fe2f2
Raidez <alexislucien72@gmail.com>
2016-02-12 10:55:16 +0100

    récupération d'un média (en dur)

--- front-end/app/views/media-view/media.js
+++ front-end/app/views/media-view/media.js
@@ -13,11 +13,10 @@ app.controller('mediaController', function ($http, $routeParam) {
 	var $ = this;
 	
 	//TODO: service get media from id
-	var url = "http://192.168.1.14:8080/resource/media.accession";
+	/*var url = "http://192.168.1.14:8080/resource/media.accession";
 	$http.get(url).then(function(res) {
 		$.media = res.data;
-	});
-	/*
+	});*/
+	
 	this.media = { "id": 10, "titre": "La petite maison dans la prairie", "auteur": "Laura Ingalss Wilder", "type": "Livre", "emprunteurs": [{ "adherent": { "id": 1, "nom": "NicolÃ¨de", "prenom": "Maitre" }, "depart": "2015-01-01 00:00:00.000", "retour": "2015-01-15 00:00:00.000" }, { "adherent": { "id": 2, "nom": "NicolÃ¨de", "prenom": "Cian" }, "depart": "2015-01-17 00:00:00.000", "retour": "2015-02-01 00:00:00.000" }, { "adherent": { "id": 3, "nom": "NicolÃ¨de", "prenom": "Cixi" }, "depart": "2015-01-17 00:00:00.000", "retour": "2015-02-01 00:00:00.000" }, { "adherent": { "id": 4, "nom": "Le troll", "prenom": "HÃ©bus" }, "depart": "2015-01-17 00:00:00.000", "retour": "2015-02-01 00:00:00.000" }] };
-	*/
 });
