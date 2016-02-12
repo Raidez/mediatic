d277856d007b0baaee1872fd17a81bbe20a97f82
Raidez <alexislucien72@gmail.com>
2016-02-12 15:30:38 +0100

    media: -design du formulaire d'ajout d'emprunteur -temporisation de la modification de média

--- front-end/app/views/media-view/media.js
+++ front-end/app/views/media-view/media.js
@@ -11,7 +11,6 @@ app.config(function ($routeProvider) {
 
 app.controller('mediaController', function ($http, $routeParams) {
 	var $ = this;
-	
 	// TODO: service get media from id
     /*
 	var url = "http://192.168.1.14:8080/resource/media.accession";
@@ -20,6 +19,19 @@ app.controller('mediaController', function ($http, $routeParams) {
         $.media.type= "CD"; //DEBUG!!
 	});*/
     
+	this.modifMedia = function() {
+		if($.media) {
+			$.media.titre = $.tmp.titre;
+			$.media.type = $.tmp.type;
+			$.media.auteur = $.tmp.auteur;
+			
+			$.tmp = angular.copy($.media);
+		}
+	};
+	this.refresh = function() {
+		$.tmp = angular.copy($.media);
+	};
+	
     this.ajoutEmprunt = function() {
         /*
         id->get
@@ -39,12 +51,11 @@ app.controller('mediaController', function ($http, $routeParams) {
                 dateRetour.setDate(dateRetour.getDate() + 15);
             }
             $.emprunt.retour = dateRetour;
-            
-            
-            
+			
             $.media.emprunteurs.push(angular.copy($.emprunt)); //ajout à la liste des emprunteurs
         }
     }
 	
 	this.media = { "id": 10, "titre": "La petite maison dans la prairie", "auteur": "Laura Ingalss Wilder", "type": "Livre", "emprunteurs": [{ "adherent": { "id": 1, "nom": "NicolÃ¨de", "prenom": "Maitre" }, "depart": "2015-01-01 00:00:00.000", "retour": "2015-01-15 00:00:00.000" }, { "adherent": { "id": 2, "nom": "NicolÃ¨de", "prenom": "Cian" }, "depart": "2015-01-17 00:00:00.000", "retour": "2015-02-01 00:00:00.000" }, { "adherent": { "id": 3, "nom": "NicolÃ¨de", "prenom": "Cixi" }, "depart": "2015-01-17 00:00:00.000", "retour": "2015-02-01 00:00:00.000" }, { "adherent": { "id": 4, "nom": "Le troll", "prenom": "HÃ©bus" }, "depart": "2015-01-17 00:00:00.000", "retour": "2015-02-01 00:00:00.000" }] };
+	this.tmp = angular.copy($.media);
 });
