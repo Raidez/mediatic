512fe2580f489aad170490ba5559dd722edc89cb
Raidez <alexislucien72@gmail.com>
2016-02-15 11:43:05 +0100

    fiche média: POST terminé (correction) + commentaire

--- front-end/app/views/media-view/media.js
+++ front-end/app/views/media-view/media.js
@@ -10,8 +10,14 @@ app.config(function ($routeProvider, $httpProvider) {
     $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF8';
 });
 
-app.controller('mediaController', function ($scope, $http, $routeParams) {
+app.controller('mediaController', function ($http, $routeParams) {
     var ctrl = this;
+	/**
+	 * utilisé pour temporiser la modification
+	 * (cela permet d'enregistrer les modifications de média
+	 * (titre, auteur, type) uniquement en cliquant sur 'enregistrer')
+	 */
+
     // TODO: service get media from id
     /*
 	var url = "http://192.168.1.14:8080/resource/media.accession";
@@ -19,27 +25,38 @@ app.controller('mediaController', function ($scope, $http, $routeParams) {
 		ctrl.media = res.data;
         ctrl.media.type= "CD"; //DEBUG!!
 	});*/
-
-    this.modifMedia = function () {
+	
+	/**
+	 * lors du click sur 'enregistrer' sur la modal de modif média
+	 * on récupère les attributs-tampon de 'tmpModifMedia'
+	 * puis on envoie les données au serveur
+	 */
+    this.applyModifMedia = function () {
         if (ctrl.media) {
-            ctrl.media.titre = ctrl.tmp.titre;
-            ctrl.media.type = ctrl.tmp.type;
-            ctrl.media.auteur = ctrl.tmp.auteur;
-            ctrl.tmp = angular.copy(ctrl.media);
+            ctrl.media.titre = ctrl.tmpModifMedia.titre;
+            ctrl.media.type = ctrl.tmpModifMedia.type;
+            ctrl.media.auteur = ctrl.tmpModifMedia.auteur;
+			
+			ctrl.refreshTmp();
+			ctrl.sendPOST();
         }
-
     };
-    this.refresh = function () {
-        ctrl.tmp = angular.copy(ctrl.media);
+	
+	// récupère une copie profonde du média
+    this.refreshTmp = function () {
+		if(ctrl.media)
+        	ctrl.tmpModifMedia = angular.copy(ctrl.media);
+    };
+	
+	// envoie la modification du média
+	this.sendPOST = function () {
+        var postUrl = "http://192.168.1.14:8080/resource/media.modification";
+        $http.post(postUrl, ctrl.media).success(function () {
+            ctrl.message = "Modification Enregistrée";
+        });
     };
 
     this.ajoutEmprunt = function () {
-        /*
-        id->get
-        date retour -> calcul
-        *nom,prénom,date d'emprunt -> set
-        
-        */
         if (ctrl.media) {
             ctrl.emprunt.adherent.id = ctrl.media.emprunteurs.length + 1; //TODO: côté serveur
             
@@ -57,23 +74,13 @@ app.controller('mediaController', function ($scope, $http, $routeParams) {
 			
             $('#ajoutEmprunteur').modal('hide');
             $('#ajoutEmprunteurForm')[0].reset();
+			
+			ctrl.sendPOST();
         }
     }
 
+	//	loader zone
     this.media = { "id": 10, "titre": "La petite maison dans la prairie", "auteur": "Laura Ingalss Wilder", "type": "Livre", "emprunteurs": [{ "adherent": { "id": 1, "nom": "NicolÃ¨de", "prenom": "Maitre" }, "depart": "2015-01-01 00:00:00.000", "retour": "2015-01-15 00:00:00.000" }, { "adherent": { "id": 2, "nom": "NicolÃ¨de", "prenom": "Cian" }, "depart": "2015-01-17 00:00:00.000", "retour": "2015-02-01 00:00:00.000" }, { "adherent": { "id": 3, "nom": "NicolÃ¨de", "prenom": "Cixi" }, "depart": "2015-01-17 00:00:00.000", "retour": "2015-02-01 00:00:00.000" }, { "adherent": { "id": 4, "nom": "Le troll", "prenom": "HÃ©bus" }, "depart": "2015-01-17 00:00:00.000", "retour": "2015-02-01 00:00:00.000" }] };
-    this.tmp = angular.copy(ctrl.media);
-
-    this.save = function () {
-      /*  var sendMedia = {
-            titre: ctrl.media.titre,
-            auteur: ctrl.media.type,
-            type: ctrl.tmp.type
-        };*/
-
-        var postUrl = "http://192.168.1.14:8080/resource/media.modification";
-        $http.post(postUrl, ctrl.media).success(function () {
-            ctrl.message = "Modification Enregistrée";
-        });
-    };
-
+    
+	ctrl.refreshTmp();
 });
