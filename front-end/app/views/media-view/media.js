2d151a67ca7d1a51bf8f9ec2b830ecb66128b358
Paul-Henry Galimberti <ph.galimberti@gmail.com>
2016-02-15 10:54:42 +0100

    fiche media ajout POST modif media

--- front-end/app/views/media-view/media.js
+++ front-end/app/views/media-view/media.js
@@ -2,63 +2,78 @@
 
 var app = angular.module('media', ['ngRoute']);
 
-app.config(function ($routeProvider) {
-	$routeProvider.when('/media', {
-		templateUrl: 'views/media-view/media.html',
-		controller: 'mediaController'
-	});
+app.config(function ($routeProvider, $httpProvider) {
+    $routeProvider.when('/media', {
+        templateUrl: 'views/media-view/media.html',
+        controller: 'mediaController'
+    });
+    $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF8';
 });
 
-app.controller('mediaController', function ($http, $routeParams, $scope) {
-	var ctrl = this;
-	// TODO: service get media from id
+app.controller('mediaController', function ($scope, $http, $routeParams) {
+    var ctrl = this;
+    // TODO: service get media from id
     /*
 	var url = "http://192.168.1.14:8080/resource/media.accession";
 	$http.get(url).then(function(res) {
 		ctrl.media = res.data;
         ctrl.media.type= "CD"; //DEBUG!!
 	});*/
-    
-	this.modifMedia = function() {
-		if(ctrl.media) {
-			ctrl.media.titre = ctrl.tmp.titre;
-			ctrl.media.type = ctrl.tmp.type;
-			ctrl.media.auteur = ctrl.tmp.auteur;
-			
-			ctrl.tmp = angular.copy(ctrl.media);
-		}
-	};
-	this.refresh = function() {
-		ctrl.tmp = angular.copy(ctrl.media);
-	};
-	
-    this.ajoutEmprunt = function() {
+
+    this.modifMedia = function () {
+        if (ctrl.media) {
+            ctrl.media.titre = ctrl.tmp.titre;
+            ctrl.media.type = ctrl.tmp.type;
+            ctrl.media.auteur = ctrl.tmp.auteur;
+            ctrl.tmp = angular.copy(ctrl.media);
+        }
+
+    };
+    this.refresh = function () {
+        ctrl.tmp = angular.copy(ctrl.media);
+    };
+
+    this.ajoutEmprunt = function () {
         /*
         id->get
         date retour -> calcul
         *nom,prénom,date d'emprunt -> set
         
         */
-        if(ctrl.media) {
+        if (ctrl.media) {
             ctrl.emprunt.adherent.id = ctrl.media.emprunteurs.length + 1; //TODO: côté serveur
             
             //calcul de la date retour selon le type de média
             var dateRetour = new Date(ctrl.emprunt.depart);
-            if(ctrl.media.type == 'Livre') {
+            if (ctrl.media.type == 'Livre') {
                 dateRetour.setDate(dateRetour.getDate() + 30);
             }
             else {
                 dateRetour.setDate(dateRetour.getDate() + 15);
             }
             ctrl.emprunt.retour = dateRetour;
-			
+
             ctrl.media.emprunteurs.push(angular.copy(ctrl.emprunt)); //ajout à la liste des emprunteurs
 			
-			$('#ajoutEmprunteur').modal('hide');
-			$('#ajoutEmprunteurForm')[0].reset();
+            $('#ajoutEmprunteur').modal('hide');
+            $('#ajoutEmprunteurForm')[0].reset();
         }
     }
-	
-	this.media = { "id": 10, "titre": "La petite maison dans la prairie", "auteur": "Laura Ingalss Wilder", "type": "Livre", "emprunteurs": [{ "adherent": { "id": 1, "nom": "NicolÃ¨de", "prenom": "Maitre" }, "depart": "2015-01-01 00:00:00.000", "retour": "2015-01-15 00:00:00.000" }, { "adherent": { "id": 2, "nom": "NicolÃ¨de", "prenom": "Cian" }, "depart": "2015-01-17 00:00:00.000", "retour": "2015-02-01 00:00:00.000" }, { "adherent": { "id": 3, "nom": "NicolÃ¨de", "prenom": "Cixi" }, "depart": "2015-01-17 00:00:00.000", "retour": "2015-02-01 00:00:00.000" }, { "adherent": { "id": 4, "nom": "Le troll", "prenom": "HÃ©bus" }, "depart": "2015-01-17 00:00:00.000", "retour": "2015-02-01 00:00:00.000" }] };
-	this.tmp = angular.copy(ctrl.media);
+
+    this.media = { "id": 10, "titre": "La petite maison dans la prairie", "auteur": "Laura Ingalss Wilder", "type": "Livre", "emprunteurs": [{ "adherent": { "id": 1, "nom": "NicolÃ¨de", "prenom": "Maitre" }, "depart": "2015-01-01 00:00:00.000", "retour": "2015-01-15 00:00:00.000" }, { "adherent": { "id": 2, "nom": "NicolÃ¨de", "prenom": "Cian" }, "depart": "2015-01-17 00:00:00.000", "retour": "2015-02-01 00:00:00.000" }, { "adherent": { "id": 3, "nom": "NicolÃ¨de", "prenom": "Cixi" }, "depart": "2015-01-17 00:00:00.000", "retour": "2015-02-01 00:00:00.000" }, { "adherent": { "id": 4, "nom": "Le troll", "prenom": "HÃ©bus" }, "depart": "2015-01-17 00:00:00.000", "retour": "2015-02-01 00:00:00.000" }] };
+    this.tmp = angular.copy(ctrl.media);
+
+    this.save = function () {
+      /*  var sendMedia = {
+            titre: ctrl.media.titre,
+            auteur: ctrl.media.type,
+            type: ctrl.tmp.type
+        };*/
+
+        var postUrl = "http://192.168.1.14:8080/resource/media.modification";
+        $http.post(postUrl, ctrl.media).success(function () {
+            ctrl.message = "Modification Enregistrée";
+        });
+    };
+
 });
