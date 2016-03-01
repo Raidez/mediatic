1ffd0fbc586cbdb795e1db99408e6102805d64f7
hverite <veritehugo@gmail.com>
2016-03-01 09:42:43 +0100

    commit inutil

---
+++ models_management/src/main/java/util/datesManagement/LocalDateConverter.java
@@ -0,0 +1,18 @@
+package util.datesManagement;
+
+import java.time.LocalDate;
+import java.sql.Date;
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter(autoApply = true)
+public class LocalDateConverter implements AttributeConverter<LocalDate, Date> {
+
+    public Date convertToDatabaseColumn(LocalDate date) {
+        return Date.valueOf(date);
+    }
+
+    public LocalDate convertToEntityAttribute(Date value) {
+        return value.toLocalDate();
+    }
+}
